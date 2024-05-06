d7c647a3e0bf2728653a0cef84fa4c18048a6c91
nowels033 <noeldomse@gmail.com>
2024-05-06 20:25:01 +0200

    update 06/05/2024 20:58

--- src/main/java/baseDeDatos/Retos/reto1/Ej3.java
+++ src/main/java/baseDeDatos/Retos/reto1/Ej3.java
@@ -19,5 +19,50 @@ Sea la base de datos llamada DatosCoches, compuesta de dos tablas Propietarios y
         Finalmente, se escribirá en pantalla la tabla actualizada
 */
 
+import java.sql.*;
+
 public class Ej3 {
+
+    public static void main(String[] args) {
+        final String instSQLSelect = "select * from personasPaises";
+        String instSQLInsert = "INSERT into productos values (4,'pistola',500.99,true)";
+        final String instSQLDelete = "delete from productos where codigo=4";
+        final String instSQLUpdate = "update personaspaises set edad = edad + 1 where nombrepais ='costa rica'";
+        final String instSQLTable = "Create table segurocoche (dni varchar(10),edad int, matricula varchar(10),seguro double)";
+        try {
+            //Class.forName("com.mysql.jbdc.driver");
+
+            Connection miConexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/datoscoches", "root", "admin");
+            Statement st = miConexion.createStatement();
+            //st.executeUpdate(instSQLInsert);
+            //st.executeUpdate(instSQLDelete);
+            //st.execute(instSQLUpdate);
+            // String insertarDatosSQL = "INSERT INTO segurocoche (dni,edad,matricula,seguro) " +
+            //    "SELECT dni,matricula " +
+            //   "FROM  propietarios JOIN coches  ON propietarios.dni = coches.dni";
+
+           st.execute(instSQLTable);
+
+            // st.execute(insertarDatosSQL);
+
+
+            ResultSet rs = st.executeQuery(instSQLSelect);
+
+            while (rs.next()) {
+
+
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private static double calcularSeguro(int edad, double precioCoche) {
+        if (edad < 40) {
+            return precioCoche * 0.02; // 2% del precio del coche
+        } else {
+            return precioCoche * 0.01; // 1% del precio del coche
+        }
+    }
+
 }
